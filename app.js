42fe780971721f4c6aee52e8ac8bdbebc86ffe82
Kévin <kevinL5@users.noreply.github.com>
2018-02-06 16:33:52 +0100

    add mongoDB and create delete function

--- app.js
+++ app.js
@@ -2,52 +2,71 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var multer  = require('multer')
 var upload = multer({ dest: 'public/uploads/' })
+var mongoose = require('mongoose')
 
 var find = require('lodash.find');
 
 var app = express()
-var generalid = 3
+mongoose.connect("mongodb://localhost:27017/leboncoin")
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 
+var productSchema = new mongoose.Schema({
+    user: String,
+    title: String,
+    city: String,
+    price: Number,
+    description: String,
+    photos: [String],
+    show: Boolean
+})
 
-var products = [
-    {
-        id: 0,
-        title: "Divers tupperware",
-        city: "Rive-de-Gier",
-        price: "15",
-        photo: "8dae02fdde5f257a8d159d59a59cbf69"
-    },
-    {
-        id: 1,
-        title: "Divers tupperware 2",
-        city: "Rive-de-Gier",
-        price: "15",
-        pseudo: "jeandu74",
-        photo: "8dae02fdde5f257a8d159d59a59cbf69",
-        description: "Un super tupperware"
-    },
-    {
-        id: 2,
-        title: "Divers tupperware 3",
-        city: "Rive-de-Gier",
-        price: "15",
-        photo: "8dae02fdde5f257a8d159d59a59cbf69"
-    }
-]
+var userSchema = new mongoose.Schema({
+    pseudo: String,
+    email: String,
+    phone: String,
+    products: [String]
+})
+
+var Product = mongoose.model("Product", productSchema)
+var User = mongoose.model("User", userSchema)
+
+// app.get('/annonce/:id/edit', function(req, res) {
+//     var id = req.params.id
+
+//     Product.findById(id, function(err, product) {
+//         if (!err) { 
+            
+//         }
+//     })
+// })
+
+app.post('/annonce/:id/delete', function(req, res) {
+    var id = req.params.id
+
+    Product.findById(id, function(err, product) {
+        if (!err) { 
+            product.show = false
+            product.save(function(err, obj) {
+                res.redirect('/')
+            })   
+        }
+    })
+})
 
 app.get('/annonce/:id', function(req, res) {
     var id = req.params.id
 
-    var product = find(products, ['id', parseInt(id)])
-
-    console.log("id " + id)
-    console.log(product)
-
-    res.render('detailsProduct.ejs', { product })
-    //res.send('coucou')
+    Product.findById(id, function(err, product) {
+        if (!err) { 
+            User.findById(product.user, function(err, user) {
+                if (!err) {
+                    res.render('detailsProduct.ejs', { product, user })
+                }
+            })
+        }
+    })
 })
 
 app.get('/deposer', function(req, res) {
@@ -55,24 +74,44 @@ app.get('/deposer', function(req, res) {
 })
 
 app.post('/deposer', upload.single("photo"), function(req, res) {
-    var newProduct = {
-        id: generalid++,
+    var product = new Product({
         title: req.body.title,
         city: req.body.city,
         price: req.body.price,
         pseudo: req.body.pseudo,
         description: req.body.description,
-        photo: req.file.filename
-    }
+        show: true
+    })
+    if (req.file) product.photos = [req.file.filename]
 
-    products = [...products, newProduct]
+    var user = new User({
+        pseudo: req.body.pseudo,
+        email: req.body.email,
+        phone: req.body.phone,
+        products: [product._id]
+    })
 
-    res.redirect('/')
+    product.user = user._id
+
+    user.save(function(err, obj) {
+        if(!err) {
+            product.save(function(err, obj) {
+                if(!err) {
+                    res.redirect('/annonce/' + product._id)
+                }
+            })
+        }
+    })
+
+    
 })
 
 app.get('/', function(req,res) {
-    console.log(products)
-    res.render('home.ejs', { products })
+    Product.find({ show: true }, function(err, products) {
+        if(!err) {
+            res.render('home.ejs', { products })
+        }
+    }) 
 })
 
 app.listen(3000, function() {
