1ada54f48242e1d3c1961141f16bb5133610dd95
Kévin <kevinL5@users.noreply.github.com>
2018-02-16 18:49:37 +0100

    upload on cloudinary

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-var multer  = require('multer')
-var upload = multer({ dest: 'public/uploads/' })
+var uniqid = require('uniqid')
+var multer = require('multer')
+var cloudinary = require('cloudinary')
+var cloudinaryStorage = require('multer-storage-cloudinary')
 var mongoose = require('mongoose')
 var session = require('express-session')
 var MongoStore = require('connect-mongo')(session)
-var passport = require('passport');
+var passport = require('passport')
 var LocalStrategy = require('passport-local')
 var passportLocalMongoose = require('passport-local-mongoose')
 
@@ -15,18 +17,40 @@ var Product = require('./models/product')
 
 var app = express()
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/nom-du-projet")
+mongoose.connect(
+	process.env.MONGODB_URI || 'mongodb://localhost:27017/leboncoin'
+)
+
+cloudinary.config({
+	cloud_name: 'vibein',
+	api_key: '994138798862576',
+	api_secret: 'D2qxMb_GF8WBNqwp7nmA5lY4YQw'
+})
+
+var storage = cloudinaryStorage({
+	cloudinary: cloudinary, // configuration de cloudinary avec vos credentials
+	folder: 'leboncoin-app', // nom du répertoire dans lequel les images vont arriver
+	allowedFormats: ['jpg', 'png'], // types de fichiers acceptés
+	transformation: { quality: 'auto' },
+	filename: function(req, file, cb) {
+		console.log(file)
+		cb(undefined, uniqid()) // génère un nom de fichier unique
+	}
+})
+var parser = multer({ storage: storage })
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // Activer la gestion de la session
-app.use(session({
-    secret: 'leboncoin-app',
-    resave: false,
-    saveUninitialized: false,
-    store: new MongoStore({mongooseConnection: mongoose.connection})
-}))
+app.use(
+	session({
+		secret: 'leboncoin-app-lknfxdszazertyuioknbvcdsazety097654é',
+		resave: false,
+		saveUninitialized: false,
+		store: new MongoStore({ mongooseConnection: mongoose.connection })
+	})
+)
 
 // Activer `passport`
 app.use(passport.initialize())
@@ -36,242 +60,240 @@ passport.use(User.createStrategy())
 passport.serializeUser(User.serializeUser()) // JSON.stringify
 passport.deserializeUser(User.deserializeUser()) // JSON.parse
 
-
 var resultsPerPage = 3
 
-app.get("/deconnexion", function(req, res) {
-    req.logout();
-    res.redirect('/');
-});
-
-app.get('/connexion', (req,res) => {
-    var user = (req.user) ? req.user : {}
-    res.render('login.ejs', { user }) 
+app.get('/deconnexion', function(req, res) {
+	req.logout()
+	res.redirect('/')
 })
 
-app.post('/connexion', passport.authenticate('local', {
-    successRedirect: '/moncompte',
-    failureRedirect: '/connexion'
-  }));
+app.get('/connexion', (req, res) => {
+	var user = req.user ? req.user : {}
+	res.render('login.ejs', { user })
+})
 
-app.get('/moncompte', (req,res) => {
-    if(req.user) {
-        var user = req.user
+app.post(
+	'/connexion',
+	passport.authenticate('local', {
+		successRedirect: '/moncompte',
+		failureRedirect: '/connexion'
+	})
+)
 
-        Product.find({user: req.user._id}, (err, products) => {
-            res.render('myAccount.ejs', { products, user })
-        })
-    }
+app.get('/moncompte', (req, res) => {
+	if (req.user) {
+		var user = req.user
+
+		Product.find({ user: req.user._id }, (err, products) => {
+			res.render('myAccount.ejs', { products, user })
+		})
+	}
 })
 
 app.get('/demandes', (req, res) => {
-    var user = (req.user) ? req.user : {}
-    var currentPage = (req.query.page) ? req.query.page : 1
-    var type = 'demandes'
+	var user = req.user ? req.user : {}
+	var currentPage = req.query.page ? req.query.page : 1
+	var type = 'demandes'
 
-    if (req.query.f) {
-        var query = { who: req.query.f, type: 'request', show: true }
-        var f = req.query.f
-    }  else { 
-        var query = { type: 'request', show: true }
-        var f = null
-    }
+	if (req.query.f) {
+		var query = { who: req.query.f, type: 'request', show: true }
+		var f = req.query.f
+	} else {
+		var query = { type: 'request', show: true }
+		var f = null
+	}
 
-    Product.count(query, (err, count) => {
-        var numberPages = Math.ceil(count/resultsPerPage)
+	Product.count(query, (err, count) => {
+		var numberPages = Math.ceil(count / resultsPerPage)
 
-        Product.find(query)
-        .limit(resultsPerPage)
-        .skip(currentPage * resultsPerPage - resultsPerPage)
-        .exec(function(err, products) {
-            if(!err) {
-                res.render('home.ejs', { 
-                    products, 
-                    type, 
-                    currentPage, 
-                    numberPages,
-                    f,
-                    user
-                })
-            }
-        })
-
-    })
+		Product.find(query)
+			.limit(resultsPerPage)
+			.skip(currentPage * resultsPerPage - resultsPerPage)
+			.exec(function(err, products) {
+				if (!err) {
+					res.render('home.ejs', {
+						products,
+						type,
+						currentPage,
+						numberPages,
+						f,
+						user
+					})
+				}
+			})
+	})
 })
 
-app.post('/annonce/:id/edit', upload.array('photos', 3), (req, res) => {
-    var id = req.params.id
+app.post('/annonce/:id/edit', parser.array('photos', 3), (req, res) => {
+	var id = req.params.id
 
-    if (req.user) {
-        Product.findById(id, (err, product) => {
-            if (!err) {
-                if ( !req.user._id == product.user ) { 
-                    return res.send('Biatch') 
-                }
+	if (req.user) {
+		Product.findById(id, (err, product) => {
+			if (!err) {
+				if (!req.user._id == product.user) {
+					return res.send('Biatch')
+				}
 
-                product.title = req.body.title
-                product.title = req.body.title
-                product.city = req.body.city
-                product.price = req.body.price
-                product.pseudo = req.body.pseudo
-                product.description = req.body.description
-                
-                if (req.files) {
-                    var filenamePhotos = []
-                    for (photo of req.files) filenamePhotos.push(photo.filename)
-                    product.photos = filenamePhotos
-                }
+				product.title = req.body.title
+				product.title = req.body.title
+				product.city = req.body.city
+				product.price = req.body.price
+				product.pseudo = req.body.pseudo
+				product.description = req.body.description
 
-                product.save(function(err, obj) {
-                    res.redirect('/annonce/' + obj._id)
-                })
-            }
-        })
-    }
+				if (req.files) {
+					var images = []
+					for (var i = 0; i < req.files.length; i++) {
+						images.push(req.files[i].secure_url)
+					}
+					product.photos = images
+				}
 
+				product.save(function(err, obj) {
+					res.redirect('/annonce/' + obj._id)
+				})
+			}
+		})
+	}
 })
 
 app.get('/annonce/:id/edit', (req, res) => {
-    var user = (req.user) ? req.user : {}
-    var id = req.params.id
+	var user = req.user ? req.user : {}
+	var id = req.params.id
 
-    Product.findById(id, (err, product) => {
-        if (!err) {
-            res.render('publishProduct.ejs', { product, user })
-        }
-    })
+	Product.findById(id, (err, product) => {
+		if (!err) {
+			res.render('publishProduct.ejs', { product, user })
+		}
+	})
 })
 
 app.post('/annonce/:id/delete', (req, res) => {
-    var id = req.params.id
+	var id = req.params.id
 
-    if (req.user) {
-        Product.findById(id, (err, product) => {
-            if (!err) {
-                if ( !req.user._id == product.user ) { 
-                    return res.send('Biatch') 
-                }
-    
-                product.show = false
-                product.save((err, obj) => {
-                    res.redirect('/')
-                })   
-            }
-        })
-    } 
-    
+	if (req.user) {
+		Product.findById(id, (err, product) => {
+			if (!err) {
+				if (!req.user._id == product.user) {
+					return res.send('Biatch')
+				}
+
+				product.show = false
+				product.save((err, obj) => {
+					res.redirect('/')
+				})
+			}
+		})
+	}
 })
 
 app.get('/annonce/:id', (req, res) => {
-    var user = (req.user) ? req.user : {}
-    var id = req.params.id
+	var user = req.user ? req.user : {}
+	var id = req.params.id
 
-    Product.findById(id, (err, product) => {
-        if (!err) { 
-            User.findById(product.user, (err, depositor) => {
-                if (!err) {
-                    res.render('detailsProduct.ejs', 
-                    { product, depositor, user })
-                }
-            })
-        }
-    })
+	Product.findById(id, (err, product) => {
+		if (!err) {
+			User.findById(product.user, (err, depositor) => {
+				if (!err) {
+					res.render('detailsProduct.ejs', { product, depositor, user })
+				}
+			})
+		}
+	})
 })
 
 app.get('/deposer', (req, res) => {
-    var user = (req.user) ? req.user : {}
-    res.render('publishProduct.ejs', { product: null, user })
+	var user = req.user ? req.user : {}
+	res.render('publishProduct.ejs', { product: null, user })
 })
 
-app.post('/deposer', upload.array('photos', 3), (req, res) => {
+app.post('/deposer', parser.array('photos', 3), (req, res) => {
+	var product = new Product({
+		who: req.body.who,
+		type: req.body.type,
+		title: req.body.title,
+		city: req.body.city,
+		price: req.body.price,
+		description: req.body.description,
+		show: true
+	})
+	if (req.files) {
+		var images = []
+		for (var i = 0; i < req.files.length; i++) {
+			images.push(req.files[i].secure_url)
+		}
+		product.photos = images
+	}
 
-    var product = new Product({
-        who: req.body.who,
-        type: req.body.type,
-        title: req.body.title,
-        city: req.body.city,
-        price: req.body.price,
-        description: req.body.description,
-        show: true
-    })
-    if (req.files) {
-        var filenamePhotos = []
-        for (photo of req.files) filenamePhotos.push(photo.filename)
-        product.photos = filenamePhotos
-    }
+	if (req.user) {
+		saveAndRedirect(req.user._id)
+	} else {
+		User.register(
+			new User({
+				pseudo: req.body.pseudo,
+				email: req.body.email,
+				password: req.body.password,
+				phone: req.body.phone,
+				products: [product._id]
+			}),
+			req.body.password, // password will be hashed
+			function(err, user) {
+				if (err) {
+					console.log(err)
+					return res.redirect('/deposer')
+				} else {
+					passport.authenticate('local')(req, res, function() {
+						saveAndRedirect(req.user._id)
+					})
+				}
+			}
+		)
+	}
 
-    if (req.user) {
-        saveAndRedirect(req.user._id)
-    } else {
-        User.register(
-            new User({
-                pseudo: req.body.pseudo,
-                email: req.body.email,
-                password: req.body.password,
-                phone: req.body.phone,
-                products: [product._id]
-            }),
-            req.body.password, // password will be hashed
-            function(err, user) {
-                if (err) {
-                    console.log(err)
-                    return res.redirect('/deposer')
-                } else {
-                    passport.authenticate('local')(req, res, function() {
-                        saveAndRedirect(req.user._id)
-                    })
-                }
-            }
-        )
-    }
-   
-    function saveAndRedirect(userid) {
-        product.user = userid
-        product.save( (err, product) => {
-            if(!err) {
-                return res.redirect('/annonce/' + product._id)
-            }
-        })
-    }
-    
+	function saveAndRedirect(userid) {
+		product.user = userid
+		product.save((err, product) => {
+			if (!err) {
+				return res.redirect('/annonce/' + product._id)
+			}
+		})
+	}
 })
 
 app.get('/', (req, res) => {
-    var user = (req.user) ? req.user : {}
-    var currentPage = (req.query.page) ? req.query.page : 1
-    var type = null
+	var user = req.user ? req.user : {}
+	var currentPage = req.query.page ? req.query.page : 1
+	var type = null
 
-    if (req.query.f) {
-        var query = { who: req.query.f, type: 'offer', show: true }
-        var f = req.query.f
-    }  else { 
-        var query = { type: 'offer', show: true }
-        var f = null
-    }
+	if (req.query.f) {
+		var query = { who: req.query.f, type: 'offer', show: true }
+		var f = req.query.f
+	} else {
+		var query = { type: 'offer', show: true }
+		var f = null
+	}
 
-    Product.count(query, (err, count) => {
-        var numberPages = Math.ceil(count/resultsPerPage)
+	Product.count(query, (err, count) => {
+		var numberPages = Math.ceil(count / resultsPerPage)
 
-        Product.find(query)
-        .limit(resultsPerPage)
-        .skip(currentPage * resultsPerPage - resultsPerPage)
-        .exec(function(err, products) {
-            if(!err) {
-                res.render('home.ejs', { 
-                    products, 
-                    type, 
-                    currentPage, 
-                    numberPages,
-                    f,
-                    user
-                })
-            }
-        })
-
-    })
-
+		Product.find(query)
+			.limit(resultsPerPage)
+			.skip(currentPage * resultsPerPage - resultsPerPage)
+			.exec(function(err, products) {
+				if (!err) {
+					res.render('home.ejs', {
+						products,
+						type,
+						currentPage,
+						numberPages,
+						f,
+						user
+					})
+				}
+			})
+	})
 })
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server started")
+	console.log('Server started')
 })
--- public/js/main.js
+++ public/js/main.js
@@ -1,171 +1,194 @@
-$(document).ready(function () {
+$(document).ready(function() {
+	/* Phone Button */
+	$('.phone-toggle').click(function() {
+		$('.phone-area').css({ display: 'flex' })
+	})
 
-    /* Checkbox */
+	/* Checkbox */
+	$('.type-check-input').on('change', function() {
+		$('.type-check-input')
+			.not(this)
+			.prop('checked', false)
+	})
 
-    $('.type-check-input').on('change', function() {
-        $('.type-check-input').not(this).prop('checked', false);  
-    })
+	$('.who-check-input').on('change', function() {
+		$('.who-check-input')
+			.not(this)
+			.prop('checked', false)
+	})
 
-    $('.who-check-input').on('change', function() {
-        $('.who-check-input').not(this).prop('checked', false);  
-    })
+	/* Validation */
+	function checkBoxWho() {
+		var whoChecked = $('input[name="who"]:checked').length > 0
 
-    /* Validation */
-    function checkBoxWho() {
-        var whoChecked = $('input[name="who"]:checked').length > 0
-        
-        if (!whoChecked) {
-            $('input[name="who"]').addClass('is-invalid')
-        } else {
-            $('input[name="who"]').removeClass('is-invalid')
-        }
-    }
+		if (!whoChecked) {
+			$('input[name="who"]').addClass('is-invalid')
+		} else {
+			$('input[name="who"]').removeClass('is-invalid')
+		}
+	}
 
-    function checkBoxType() {
-        var typeChecked = $('input[name="type"]:checked').length > 0
+	function checkBoxType() {
+		var typeChecked = $('input[name="type"]:checked').length > 0
 
-        if (!typeChecked) {
-            $('input[name="type"]').addClass('is-invalid')
-        } else {
-            $('input[name="type"]').removeClass('is-invalid')
-        }
-    }
+		if (!typeChecked) {
+			$('input[name="type"]').addClass('is-invalid')
+		} else {
+			$('input[name="type"]').removeClass('is-invalid')
+		}
+	}
 
-    $("input[name='title'], textarea[name='description'], input[name='price'], input[name='type']").click(function() {
-        checkBoxWho()
-        checkBoxType()
-    })
+	$(
+		"input[name='title'], textarea[name='description'], input[name='price'], input[name='type']"
+	).click(function() {
+		checkBoxWho()
+		checkBoxType()
+	})
 
-    $("input[name='who']").click(function () {
-        checkBoxWho()
-    })
+	$("input[name='who']").click(function() {
+		checkBoxWho()
+	})
 
-    function checkTitle() {
-        var selector = "input[name='title']"
-        if( $(selector).val().length < 3 || $(selector).val().length > 81) {
-            $(selector).addClass('is-invalid')
-        } else {
-            $(selector).removeClass('is-invalid').addClass('is-valid')
-        }
-    }
+	function checkTitle() {
+		var selector = "input[name='title']"
+		if ($(selector).val().length < 3 || $(selector).val().length > 81) {
+			$(selector).addClass('is-invalid')
+		} else {
+			$(selector)
+				.removeClass('is-invalid')
+				.addClass('is-valid')
+		}
+	}
 
-    $("input[name='title']").bind("blur",  function(){
-        checkTitle()
-    })
+	$("input[name='title']").bind('blur', function() {
+		checkTitle()
+	})
 
-    function checkDescription() {
-        var selector = "textarea[name='description']"
-        if( $(selector).val().length < 3 || $(selector).val().length > 1000000) {
-            $(selector).addClass('is-invalid')
-        } else {
-            $(selector).removeClass('is-invalid').addClass('is-valid')
-        }
-    }
+	function checkDescription() {
+		var selector = "textarea[name='description']"
+		if ($(selector).val().length < 3 || $(selector).val().length > 1000000) {
+			$(selector).addClass('is-invalid')
+		} else {
+			$(selector)
+				.removeClass('is-invalid')
+				.addClass('is-valid')
+		}
+	}
 
-    $("textarea[name='description']").bind("blur",  function(){
-        checkDescription()
-    })
+	$("textarea[name='description']").bind('blur', function() {
+		checkDescription()
+	})
 
-    function checkPrice() {
-        var selector = "input[name='price']"
-        if( isNaN(parseInt($(selector).val())) ) {
-            $(selector).addClass('is-invalid')
-        } else {
-            $(selector).removeClass('is-invalid').addClass('is-valid')
-        }
-    }
+	function checkPrice() {
+		var selector = "input[name='price']"
+		if (isNaN(parseInt($(selector).val()))) {
+			$(selector).addClass('is-invalid')
+		} else {
+			$(selector)
+				.removeClass('is-invalid')
+				.addClass('is-valid')
+		}
+	}
 
-    $("input[name='price']").bind("blur",  function(){
-        checkPrice()
-    })
+	$("input[name='price']").bind('blur', function() {
+		checkPrice()
+	})
 
-    function checkCity() {
-        var selector = "input[name='city']"
-        if( $(selector).val().length < 3 || $(selector).val().length > 1000) {
-            $(selector).addClass('is-invalid')
-        } else {
-            $(selector).removeClass('is-invalid').addClass('is-valid')
-        }
-    }
+	function checkCity() {
+		var selector = "input[name='city']"
+		if ($(selector).val().length < 3 || $(selector).val().length > 1000) {
+			$(selector).addClass('is-invalid')
+		} else {
+			$(selector)
+				.removeClass('is-invalid')
+				.addClass('is-valid')
+		}
+	}
 
-    $("input[name='city']").bind("blur",  function(){
-        checkCity()
-    })
+	$("input[name='city']").bind('blur', function() {
+		checkCity()
+	})
 
-    function checkPseudo() {
-        var selector = "input[name='pseudo']"
-        if( $(selector).val().length < 3 || $(selector).val().length > 51) {
-            $(selector).addClass('is-invalid')
-        } else {
-            $(selector).removeClass('is-invalid').addClass('is-valid')
-        }
-    }
+	function checkPseudo() {
+		var selector = "input[name='pseudo']"
+		if ($(selector).val().length < 3 || $(selector).val().length > 51) {
+			$(selector).addClass('is-invalid')
+		} else {
+			$(selector)
+				.removeClass('is-invalid')
+				.addClass('is-valid')
+		}
+	}
 
-    $("input[name='pseudo']").bind("blur",  function(){
-        checkPseudo()
-    })
+	$("input[name='pseudo']").bind('blur', function() {
+		checkPseudo()
+	})
 
-    function checkEmail() {
-        var selector = "input[name='email']"
-        var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+	function checkEmail() {
+		var selector = "input[name='email']"
+		var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-        if( !(regex).test( $(selector).val() ) ) {
-            $(selector).addClass('is-invalid')
-        } else {
-            $(selector).removeClass('is-invalid').addClass('is-valid')
-        }
-    }
+		if (!regex.test($(selector).val())) {
+			$(selector).addClass('is-invalid')
+		} else {
+			$(selector)
+				.removeClass('is-invalid')
+				.addClass('is-valid')
+		}
+	}
 
-    $("input[name='email']").bind("blur",  function(){
-        checkEmail()
-    })
+	$("input[name='email']").bind('blur', function() {
+		checkEmail()
+	})
 
-    function checkPassword() {
-        var selector = "input[name='password']"
-        
-        if( $(selector).val().length < 7 || $(selector).val().length > 101) {
-            $(selector).addClass('is-invalid')
-        } else {
-            $(selector).removeClass('is-invalid').addClass('is-valid')
-        }
-    }
+	function checkPassword() {
+		var selector = "input[name='password']"
 
-    $("input[name='password']").bind("blur",  function(){
-        checkPassword()
-    })
+		if ($(selector).val().length < 7 || $(selector).val().length > 101) {
+			$(selector).addClass('is-invalid')
+		} else {
+			$(selector)
+				.removeClass('is-invalid')
+				.addClass('is-valid')
+		}
+	}
 
-    function checkPhone() {
-        var selector = "input[name='phone']"
-        var regex = /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/
+	$("input[name='password']").bind('blur', function() {
+		checkPassword()
+	})
 
-        if( !(regex).test( $(selector).val() ) ) {
-            $(selector).addClass('is-invalid')
-        } else {
-            $(selector).removeClass('is-invalid').addClass('is-valid')
-        }
-    }
+	function checkPhone() {
+		var selector = "input[name='phone']"
+		var regex = /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/
 
-    $("input[name='phone']").bind("blur",  function(){
-        checkPhone()
-    })
+		if (!regex.test($(selector).val())) {
+			$(selector).addClass('is-invalid')
+		} else {
+			$(selector)
+				.removeClass('is-invalid')
+				.addClass('is-valid')
+		}
+	}
 
+	$("input[name='phone']").bind('blur', function() {
+		checkPhone()
+	})
 
-    /* Button submit */
-    $(".btn-publish").click(function(event) {
-        checkBoxWho()
-        checkBoxType()
-        checkTitle()
-        checkDescription()
-        checkPrice()
-        checkCity()
-        checkPseudo()
-        checkEmail()
-        checkPassword()
-        checkPhone()
+	/* Button submit */
+	$('.btn-publish').click(function(event) {
+		checkBoxWho()
+		checkBoxType()
+		checkTitle()
+		checkDescription()
+		checkPrice()
+		checkCity()
+		checkPseudo()
+		checkEmail()
+		checkPassword()
+		checkPhone()
 
-        if ( $('input').hasClass('is-invalid') ) {
-            event.preventDefault()
-        }
-    })
-
+		if ($('input').hasClass('is-invalid')) {
+			event.preventDefault()
+		}
+	})
 })
